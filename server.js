10c812834d9bcedf3eef495c93ece5cba7c1ce7f
achal.parikh13@gmail.com <achal.parikh@uoit.net>
2017-04-08 21:30:15 -0400

    Update, Index.html is now deault

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
-app.get('/', function(req, res){
-    res.send('hello this is a response from the server');
-});
+// app.get('/', function(req, res){
+//     res.send('hello this is a response from the server');
+// });
 
 app.get ('/about', middleware.requireAuth, function(req, res) {
     res.send('This is an about us page! WHOW');
